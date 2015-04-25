bc2c7320f3d4e211b11044d395b95e5fc3009265
Brian Cocherell <brian.cocherell@gmail.com>
2015-04-25 14:44:52 -0400

    Again, fixed some tabs...

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-		i <- NULL
-		set <- function(y) {
-			x <<- y
-			i <<- NULL
-		}
-		get <- function() x
-		setinv <- function(inv = matrix()) i <<- inv
-		getinv <- function() i
-		list(set = set, get = get,
-			setinv = setinv,
-			getinv = getinv)
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inv = matrix()) i <<- inv
+	getinv <- function() i
+	list(set = set, get = get,
+		setinv = setinv,
+		getinv = getinv)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        i <- x$getinv()
-        if(!is.null(i)) {
-			message("getting cached data")
-			return(i)
-        }
-        data <- x$get()
-        i <- solve(data, ...)
-        x$setinv(i)
-        i
+	i <- x$getinv()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setinv(i)
+	i
 }
